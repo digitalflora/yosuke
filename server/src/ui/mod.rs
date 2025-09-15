ff38865ab95b42cff88cc16512bc6b958e5dc5ab
Fly2 <vv@s1no.uk>
2025-09-15 22:28:42 +0100

    funny (in my opinion)

--- server/src/ui/mod.rs
+++ server/src/ui/mod.rs
@@ -1,5 +1,5 @@
 use eframe::{NativeOptions, run_native};
-use egui::ViewportBuilder;
+use egui::{IconData, ViewportBuilder};
 
 use crate::types::mouthpieces::UiMouthpiece;
 
@@ -12,13 +12,33 @@ mod view;
 pub fn main(mouthpiece: UiMouthpiece) -> eframe::Result<()> {
     println!("[*] ui spawned");
     let options = NativeOptions {
-        viewport: ViewportBuilder::default().with_inner_size([720.0, 560.0]),
+        viewport: ViewportBuilder::default()
+            .with_inner_size([720.0, 560.0])
+            .with_icon(load_icon()),
         ..Default::default()
     };
 
     run_native(
-        "Oxide",
+        "Yosuke",
         options,
         Box::new(move |_cc| Ok(Box::new(view::View::new(mouthpiece)))),
     )
+}
+
+fn load_icon() -> IconData {
+    let (icon_rgba, icon_width, icon_height) = {
+        let icon = include_bytes!("../../../yosuke.ico");
+        let image = image::load_from_memory(icon)
+            .expect("Failed to open icon path")
+            .into_rgba8();
+        let (width, height) = image.dimensions();
+        let rgba = image.into_raw();
+        (rgba, width, height)
+    };
+
+    eframe::egui::IconData {
+        rgba: icon_rgba,
+        width: icon_width,
+        height: icon_height,
+    }
 }
