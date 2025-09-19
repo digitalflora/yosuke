40bf730057daed056bb7f6c8d029f0aa129a6974
Fly2 <vv@s1no.uk>
2025-09-19 22:43:35 +0100

    teensy

--- build_macOS.sh
+++
@@ -1,9 +0,0 @@
-cd "$(dirname "$0")"
-rm -r target/release/bundle/osx/
-cd ./server
-cargo bundle --release # creates bundle/osx
-cd ../client
-cargo build --release --target x86_64-pc-windows-gnu
-cd ../
-mv target/x86_64-pc-windows-gnu/release/client.exe target/release/bundle/osx/stub.dat
-open -R target/release/bundle/osx/Yosuke.app
--- client/src/commands/computer_info.rs
+++ client/src/commands/computer_info.rs
@@ -1,12 +1,12 @@
 use is_elevated::is_elevated;
 use shared::commands::{ComputerInfoResponse, Response};
 use winapi::um::winbase::GetComputerNameW;
-pub fn main() -> Result<Response, Box<dyn std::error::Error>> {
 
+pub fn main() -> Result<Response, Box<dyn std::error::Error>> {
     let mut hostname_buf = [0u16; 16];
-    unsafe {GetComputerNameW(hostname_buf.as_mut_ptr(), &mut (hostname_buf.len() as u32)) };
+    unsafe { GetComputerNameW(hostname_buf.as_mut_ptr(), &mut (hostname_buf.len() as u32)) };
     Ok(Response::ComputerInfo(ComputerInfoResponse {
         hostname: String::from_utf16_lossy(&hostname_buf).to_string(),
-        elevated: is_elevated()
+        elevated: is_elevated(),
     }))
 }
